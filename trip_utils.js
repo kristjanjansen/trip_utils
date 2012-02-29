feee101fe852e56c5f47c238a346c8633a650bd6
Kristjan Jansen <kristjan.jansen@gmail.com>
2012-02-29 17:17:59 +0200

    initial checkin

---
+++ trip_utils.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+Drupal.behaviors.test = {
+  attach: function (context, settings) {
+  
+  $('.logged-in .view-test tr').each(function() {
+  
+    var nid = $(this).find('td.views-field-nid');
+  
+    $.ajaxSetup({ cache: false });
+   
+    $.getJSON(Drupal.settings.basePath + 'json/' + nid.html(), {
+      },
+      function (data, textStatus) {
+      //  console.log(data);
+        nid.parent().find('td.views-field-nothing').html(data.nodes[0].node.new_comments);
+      }
+    );
+  
+  });
+
+
+
+  }
+};
+
+})(jQuery);
