4f55f22f069212961dde30c655ab790f126ba88d
Kristjan Jansen <kristjan.jansen@gmail.com>
2012-09-08 16:09:25 +0300

    fixes

--- trip_utils.js
+++ trip_utils.js
@@ -3,20 +3,21 @@
 Drupal.behaviors.test = {
   attach: function (context, settings) {
 
+   var type = getURLParameter('type');
    var page = getURLParameter('page');
     
     $.ajaxSetup({
       cache: false
     });
     
-    $.getJSON(Drupal.settings.basePath + 'json/?page=' + page, {
+    $.getJSON(Drupal.settings.basePath + 'json/?type=' + type + '&page=' + page, {
       },
       function (data, textStatus) {
         var len = data.nodes.length;
         for (var i = 0; i < len; i++) {
           var str = data.nodes[i].node.new_comments;
           // console.log(str);
-          $('.logged-in .view-trip-test td.views-field-nothing:eq(' + i + ')')
+          $('.logged-in .view-forum td.views-field-nothing:eq(' + i + ')')
             .html(str);
         }
       }
