0472bf187d8418b3840242cfeaa9ed878f2bb970
Kristjan Jansen <kristjan.jansen@gmail.com>
2012-09-16 19:21:50 +0300

    new stylee

--- trip_utils.js
+++ trip_utils.js
@@ -16,10 +16,12 @@ Drupal.behaviors.test = {
       function (data, textStatus) {
         var len = data.nodes.length;
         for (var i = 0; i < len; i++) {
-          var str = data.nodes[i].node.new_comments;
+          if (data.nodes[i].node.new_comments > 0) {
+          var str = '<span>'+ data.nodes[i].node.new_comments + '</span>';
           // console.log(str);
           $('.logged-in .view-forums td.views-field-nothing:eq(' + i + ')')
             .html(str);
+          }
         }
       }
       );
